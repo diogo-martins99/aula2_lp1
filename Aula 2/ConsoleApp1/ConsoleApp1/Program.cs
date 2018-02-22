2c14833f0d9c8ab0ac67e508c93f45ba7789360c
Diogo Martins <diogofm99@gmail.com>
2018-02-22 15:20:17 +0000

    Exercício 6 - Math.PI

---
+++ Aula 2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double height;
+            double radius;
+            double area;
+            double volume;
+
+            Console.WriteLine("\nInsert Height:\n");
+            height = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("\nInsert Radius:\n");
+            radius = Convert.ToDouble(Console.ReadLine());
+
+            area = (2 * Math.PI * (radius * (radius + height)));
+            volume = Math.PI * (radius * radius) * height;
+
+            Console.WriteLine("\nArea:\n" + area);
+            Console.WriteLine("\nVolume:\n" + volume);
+
+
+
+        }
+    }
+}
